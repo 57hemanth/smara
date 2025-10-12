afbae802546faefe5a187e3701e9b84a0c990e2c
57hemanth <hemanthchowdary1234@gmail.com>
2025-10-12 14:47:15 +0530

    feat: implemented text to embedding worker

---
+++ workers/text_to_embedding/index.ts
@@ -0,0 +1,57 @@
+import { json } from "@smara/shared/utils";
+
+export interface Env {
+    AI: Ai;
+    VECTORIZE: Vectorize;
+}
+
+const MODEL = "@cf/google/embeddinggemma-300m"
+
+export default {
+    async fetch(request: Request, env: Env): Promise<Response> {
+        try {
+            if (request.method !== "POST") {
+                return json({ error: "Use POST" }, 405);
+            }
+
+            const body = (await request.json()) as { text: string, user_id: string, asset_id: string };
+
+            if (!body.text) {
+                return json({ error: "Provide a text" }, 400);
+            }
+
+            const aiRes = await env.AI.run(
+                MODEL,
+                {
+                    text: body.text
+                }
+            ) as any;
+
+            const vectorizeRes = await env.VECTORIZE.upsert([
+                {
+                    id: body.asset_id,
+                    values: aiRes?.data?.[0],
+                    metadata: {
+                        text: body.text,
+                        user_id: body.user_id,
+                        modality: "text",
+                        date: new Date().toISOString(),
+                    },
+                }
+            ]);
+
+            console.log("===================================");
+            console.log(vectorizeRes);
+            console.log("===================================");
+
+            return json({
+                model: MODEL,
+                message: "Success"
+            }, 200);
+        }
+        catch (err: any) {
+            console.error(err);
+            return json({ error: err?.message || "Internal error" }, 500);
+        }
+    }
+} satisfies ExportedHandler<Env>;
