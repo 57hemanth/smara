0e07c34f859f8bc62002dd32bf22fec08569f5f2
57hemanth <hemanthchowdary1234@gmail.com>
2025-10-12 14:58:55 +0530

    feat: implemented search functionality worker

---
+++ workers/search/index.ts
@@ -0,0 +1,45 @@
+import { json } from "@smara/shared/utils";
+
+export interface Env {
+    AI: Ai;
+    VECTORIZE: Vectorize;
+}
+const MODEL = "@cf/google/embeddinggemma-300m"
+
+type ReqBody = {
+    query: string;
+    topK: number;
+    user_id: string;
+}
+
+export default{
+    async fetch(request: Request, env: Env): Promise<Response> {
+        try {
+            if (request.method !== "POST") {
+                return json({ error: "Use POST" }, 405);
+            }
+
+            const body = (await request.json()) as ReqBody;
+            
+            const queryVector = await env.AI.run(
+                MODEL,
+                {
+                    text: body.query
+                }
+            ) as any;
+
+            const vectorizeRes = await env.VECTORIZE.query(queryVector.data[0], {
+                topK: body.topK || 10,
+                filter: {
+                    user_id: body.user_id
+                }
+            });
+
+            return json({ data: vectorizeRes }, 200);
+        }
+        catch (err: any) {
+            console.error(err);
+            return json({ error: err?.message || "Internal error" }, 500);
+        }
+    }
+}
